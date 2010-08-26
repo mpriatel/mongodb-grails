45ad95e1b2ec1561abe0a05f3b4eb23dc65b1f9c
Mark Priatel <mark@iolog.com>
2010-08-26 16:09:31 -0400

    .

---
+++ test/unit/com/iolog/mongodbtools/MongoStatusControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.iolog.mongodbtools
+
+import grails.test.*
+
+class MongoStatusControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
